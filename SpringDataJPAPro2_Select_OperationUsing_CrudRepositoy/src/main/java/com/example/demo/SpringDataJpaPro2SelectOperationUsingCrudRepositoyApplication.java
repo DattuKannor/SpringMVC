84ae2fd59dfe00461dc5cad9b535a7ee4c9cbf9d
Hp <Hp@192.168.1.15>
2021-07-17 18:01:37 +0530

    initial pro2

---
+++ SpringDataJPAPro2_Select_OperationUsing_CrudRepositoy/src/main/java/com/example/demo/SpringDataJpaPro2SelectOperationUsingCrudRepositoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPro2SelectOperationUsingCrudRepositoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPro2SelectOperationUsingCrudRepositoyApplication.class, args);
+	}
+
+}
