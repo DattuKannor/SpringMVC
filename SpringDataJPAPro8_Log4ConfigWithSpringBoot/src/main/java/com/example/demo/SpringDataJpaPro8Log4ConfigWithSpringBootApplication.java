4e1b7173592117e341cc3d65c86f77cc52fb70c1
Hp <Hp@192.168.1.15>
2021-07-28 13:12:30 +0530

    Initial commit

---
+++ SpringDataJPAPro8_Log4ConfigWithSpringBoot/src/main/java/com/example/demo/SpringDataJpaPro8Log4ConfigWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPro8Log4ConfigWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPro8Log4ConfigWithSpringBootApplication.class, args);
+	}
+
+}
