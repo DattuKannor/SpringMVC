d6ecc4794e663beb66863eba25910c6a9d9c4a0f
Hp <Hp@192.168.1.15>
2021-07-28 13:18:48 +0530

    intial

---
+++ SpringDataJPAPro8_Log4ConfigWithSpringBoot/src/main/java/com/example/demo/runner/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.example.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DemoRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
